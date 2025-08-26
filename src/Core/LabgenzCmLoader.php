6ce5b545be931b95aea47abac0c3e330d1f242c3
Yahya Cotton <yahyadard@gmail.com>
2025-08-26 12:31:24 -0700

    feat: Update component definitions in LabgenzCmLoader for improved organization and new authentication features

--- src/Core/LabgenzCmLoader.php
+++ src/Core/LabgenzCmLoader.php
@@ -64,15 +64,20 @@ class LabgenzCmLoader {
 	 */
 	private function define_components(): void {
 		$this->components = [
-			'core.assets'                   => \LABGENZ_CM\Core\AssetsManager::class,
-			'core.menu'                     => \LABGENZ_CM\Core\MenuManager::class,
-			'core.settings'                 => \LABGENZ_CM\Core\Settings::class,
-			'core.ajax'                     => \LABGENZ_CM\Core\AjaxHandler::class,
-			'core.woo_helper'               => \LABGENZ_CM\Core\WooCommerceHelper::class,
-			'core.profile_location_handler' => \LABGENZ_CM\Core\ProfileLocationHandler::class,
-			'core.organization_access'      => \LABGENZ_CM\Core\OrganizationAccess::class,
-			'core.page_access_controller'   => \LABGENZ_CM\Core\PageAccessController::class,
-			'core.user_menu_handler'        => \LABGENZ_CM\Core\UserMenuHandler::class,
+			'core.assets'                             => \LABGENZ_CM\Core\AssetsManager::class,
+			'core.menu'                               => \LABGENZ_CM\Core\MenuManager::class,
+			'core.settings'                           => \LABGENZ_CM\Core\Settings::class,
+			'core.ajax'                               => \LABGENZ_CM\Core\AjaxHandler::class,
+			'core.woo_helper'                         => \LABGENZ_CM\Core\WooCommerceHelper::class,
+			'core.profile_location_handler'           => \LABGENZ_CM\Core\ProfileLocationHandler::class,
+			'core.organization_access'                => \LABGENZ_CM\Core\OrganizationAccess::class,
+			'core.page_access_controller'             => \LABGENZ_CM\Core\PageAccessController::class,
+			'core.user_menu_handler'                  => \LABGENZ_CM\Core\UserMenuHandler::class,
+			'core.gettext_overrides'                  => \LABGENZ_CM\Core\Localization\GettextOverrides::class,
+			'core.authentication.multi_email_manager' => \LABGENZ_CM\Core\Authentication\MultiEmailManager::class,
+			'core.authentication.email_authenticator' => \LABGENZ_CM\Core\Authentication\EmailAuthenticator::class,
+			'core.authentication.account.wc_page' 	  => \LABGENZ_CM\Core\Authentication\Account\AccountPageHandler::class,
+			'core.authentication.profile.wp_profile'  => \LABGENZ_CM\Core\Authentication\Profile\WpProfile::class,
 
 			/**
 			 * @deprecated components
@@ -82,37 +87,37 @@ class LabgenzCmLoader {
 			// 'core.invite_handler'        => \LABGENZ_CM\Core\InviteHandler::class,
 			// 'core.remove_handler'        => \LABGENZ_CM\Core\RemoveHandler::class,
 
-			'database.database'             => \LABGENZ_CM\Database\Database::class,
+			'database.database'                       => \LABGENZ_CM\Database\Database::class,
 
-			'admin.organization_access'     => \LABGENZ_CM\Admin\OrganizationAccessAdmin::class,
-			'admin.weekly_articles_admin'   => \LABGENZ_CM\Admin\DailyArticleAdmin::class,
-			'admin.reviews'                 => \LABGENZ_CM\Admin\ReviewsAdmin::class,
-			'admin.subscription_admin'      => \LABGENZ_CM\Admin\SubscriptionsAdmin::class,
+			'admin.organization_access'               => \LABGENZ_CM\Admin\OrganizationAccessAdmin::class,
+			'admin.weekly_articles_admin'             => \LABGENZ_CM\Admin\DailyArticleAdmin::class,
+			'admin.reviews'                           => \LABGENZ_CM\Admin\ReviewsAdmin::class,
+			'admin.subscription_admin'                => \LABGENZ_CM\Admin\SubscriptionsAdmin::class,
 
-			'public.organization_access'    => \LABGENZ_CM\Public\OrganizationAccessPublic::class,
+			'public.organization_access'              => \LABGENZ_CM\Public\OrganizationAccessPublic::class,
 
-			'xprofile.field_type_handler'   => \LABGENZ_CM\XProfile\XProfileFieldTypeHandler::class,
+			'xprofile.field_type_handler'             => \LABGENZ_CM\XProfile\XProfileFieldTypeHandler::class,
 
-			'groups.create_handler'         => \LABGENZ_CM\Groups\GroupCreationHandler::class,
-			'groups.member_handler'         => \LABGENZ_CM\Groups\GroupMembersHandler::class,
-			'groups.manage_members_tab'     => \LABGENZ_CM\Groups\ManageMembersTab::class,
-			'groups.members_map_handler'    => \LABGENZ_CM\Groups\MembersMapHandler::class,
-			'groups.member_visibility'      => \LABGENZ_CM\Groups\GroupMemberVisibility::class,
-			'groups.filters_handler'        => \LABGENZ_CM\Groups\GroupFiltersHandler::class,
+			'groups.create_handler'                   => \LABGENZ_CM\Groups\GroupCreationHandler::class,
+			'groups.member_handler'                   => \LABGENZ_CM\Groups\GroupMembersHandler::class,
+			'groups.manage_members_tab'               => \LABGENZ_CM\Groups\GroupTabs::class,
+			'groups.members_map_handler'              => \LABGENZ_CM\Groups\MembersMapHandler::class,
+			'groups.member_visibility'                => \LABGENZ_CM\Groups\GroupMemberVisibility::class,
+			'groups.filters_handler'                  => \LABGENZ_CM\Groups\GroupFiltersHandler::class,
 
-			'articles.articles_post_type'   => \LABGENZ_CM\Articles\ArticlesPostType::class,
-			'articles.core_handler'         => \LABGENZ_CM\Articles\ArticlesHandler::class,
-			'articles.weekly_handler'       => \LABGENZ_CM\Articles\DailyArticleHandler::class,
-			'articles.reviews_handler'      => \LABGENZ_CM\Articles\ReviewsHandler::class,
-			'articles.download_handler'     => \LABGENZ_CM\Articles\SingleArticleHandler::class,
-			'articles.card_display'         => \LABGENZ_CM\Articles\ArticleCardDisplayHandler::class,
+			'articles.articles_post_type'             => \LABGENZ_CM\Articles\ArticlesPostType::class,
+			'articles.core_handler'                   => \LABGENZ_CM\Articles\ArticlesHandler::class,
+			'articles.weekly_handler'                 => \LABGENZ_CM\Articles\DailyArticleHandler::class,
+			'articles.reviews_handler'                => \LABGENZ_CM\Articles\ReviewsHandler::class,
+			'articles.download_handler'               => \LABGENZ_CM\Articles\SingleArticleHandler::class,
+			'articles.card_display'                   => \LABGENZ_CM\Articles\ArticleCardDisplayHandler::class,
 
-			'gamipress.header_integration'  => \LABGENZ_CM\Gamipress\GamiPressHeaderIntegration::class,
-			'gamipress.currency_handler'    => \LABGENZ_CM\Gamipress\GamipressCurrencyHandler::class,
+			'gamipress.header_integration'            => \LABGENZ_CM\Gamipress\GamiPressHeaderIntegration::class,
+			'gamipress.currency_handler'              => \LABGENZ_CM\Gamipress\GamipressCurrencyHandler::class,
 
-			'subscription.handler'          => \LABGENZ_CM\Subscriptions\SubscriptionHandler::class,
-			'subscription.shortcode'        => \LABGENZ_CM\Subscriptions\Shortcodes\SubscriptionDetailsShortcode::class,
-			'widgets.widgets.elementor'     => \LABGENZ_CM\Widgets\Elementor\ElementorIntegration::class,
+			'subscription.handler'                    => \LABGENZ_CM\Subscriptions\SubscriptionHandler::class,
+			'subscription.shortcode'                  => \LABGENZ_CM\Subscriptions\Shortcodes\SubscriptionDetailsShortcode::class,
+			'widgets.widgets.elementor'               => \LABGENZ_CM\Widgets\Elementor\ElementorIntegration::class,
 		];
 	}
 
